077423311f27f1cb57069b24defa611e18c7a641
Karim Fadl <kareemfadl10@gmail.com>
2019-05-16 19:53:13 +0200

    Add Mysql backup file

---
+++ db-backup/image/perform-backup.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+
+
+# Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
+has_failed=false
+
+
+# Loop through all the defined databases, seperating by a ,
+for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }
+do
+
+    # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
+    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $CURRENT_DATABASE 2>&1 > /tmp/$CURRENT_DATABASE-$(date "+%b-%d-%Y-%H-%M").sql)
+    then
+        
+        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+
+        # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
+        if awsoutput=$(aws s3 cp /tmp/$CURRENT_DATABASE-$(date "+%b-%d-%Y-%H-%M").sql s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH/$CURRENT_DATABASE$(date "+%b-%d-%Y-%H-%M").sql 2>&1)
+        then
+            echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+        else
+            echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/aws-database-backup.log
+            has_failed=true
+        fi
+
+    else
+        echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/aws-database-backup.log
+        has_failed=true
+    fi
+
+done
